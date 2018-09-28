d36d73948fc95de3679a5e558dfa15070da014bc
ethanostrow <ethanekat@gmail.com>
2018-09-27 22:15:23 -0700

    added colors, summation

--- build/source/Dice.java
+++ build/source/Dice.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die bob;
 int sum;
 int filter;
-void setup(){
+public void setup(){
 	noLoop();
-	size(825,900);
+	
 
 }
-void draw(){
+public void draw(){
 	background(255);
 	sum = 0;
 	filter = 255;
@@ -22,7 +38,7 @@ void draw(){
 	textSize(20);
 	text("sum:" + sum, 825/2 - 40, 895);
 }
-void mousePressed(){
+public void mousePressed(){
 	redraw();
 }
 class Die{
@@ -36,10 +52,10 @@ class Die{
 		blue = b;
 
 	}
-	void roll(){
+	public void roll(){
 		dots = (int)(Math.random()*6) + 1;
 	}
-	void show(){
+	public void show(){
 		fill(red, green, blue);
 		rect(myX, myY, 55, 55, 6);
 		stroke(red, green - 50, 255);
@@ -84,4 +100,14 @@ class Die{
 		}
 
 	}
+}
+  public void settings() { 	size(825,900); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
